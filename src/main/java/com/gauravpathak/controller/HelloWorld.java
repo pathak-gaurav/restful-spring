49dc88286bd1a80463e7733f4e2b880e11b0691e
gauravpathak <gauravpathak.spring@gmail.com>
2019-03-23 21:10:19 +0530

    Added cross origin

--- src/main/java/com/gauravpathak/controller/HelloWorld.java
+++ src/main/java/com/gauravpathak/controller/HelloWorld.java
@@ -1,11 +1,13 @@
 package com.gauravpathak.controller;
 
 import com.gauravpathak.model.HelloWorldBean;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class HelloWorld {
 
     @GetMapping("/hello-world/{message}")
