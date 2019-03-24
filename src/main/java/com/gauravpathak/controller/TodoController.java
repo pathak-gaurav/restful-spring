22df189abe2182af7c285002f6df2d0dbd447dd6
gauravpathak <gauravpathak.spring@gmail.com>
2019-03-24 21:49:31 +0530

    Added Cross Origin

--- src/main/java/com/gauravpathak/controller/TodoController.java
+++ src/main/java/com/gauravpathak/controller/TodoController.java
@@ -2,15 +2,13 @@ package com.gauravpathak.controller;
 
 import com.gauravpathak.model.Todo;
 import com.gauravpathak.repository.TodoRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:4200")
 public class TodoController {
 
     private TodoRepository repository;
