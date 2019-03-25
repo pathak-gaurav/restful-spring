45b71d721af2c6081e5019e3e68f642004148db1
gauravpathak <gauravpathak.spring@gmail.com>
2019-03-25 22:14:58 +0530

    Added Get Mapping to get Single Record

--- src/main/java/com/gauravpathak/controller/TodoController.java
+++ src/main/java/com/gauravpathak/controller/TodoController.java
@@ -2,9 +2,12 @@ package com.gauravpathak.controller;
 
 import com.gauravpathak.model.Todo;
 import com.gauravpathak.repository.TodoRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -20,5 +23,24 @@ public class TodoController {
     @GetMapping(path = "/users/{username}/todos")
     public List<Todo> findAllTodo(@PathVariable("username") String username) {
         return repository.findAll();
+    }
+
+    @DeleteMapping(path = "/users/{username}/todos/{id}")
+    public ResponseEntity deleteById(@PathVariable("username") String username, @PathVariable("id") Long id) {
+        if (repository.findById(id).orElse(null) == null) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+        repository.deleteById(id);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @GetMapping("/users/{username}/todos/{id}")
+    public ResponseEntity findById(@PathVariable("username") String username, @PathVariable("id") Long id) {
+        if (repository.findById(id).orElse(null) == null) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        } else {
+            Optional<Todo> todo = repository.findById(id);
+            return new ResponseEntity(todo, HttpStatus.OK);
+        }
     }
 }
